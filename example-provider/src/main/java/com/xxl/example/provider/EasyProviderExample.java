6aabb5110862d745d88eba7c20cf48bfe7cae91e
xu_xiaolong <luckily126163@126.com>
2024-03-08 17:21:34 +0800

    开发简易版rpc框架

--- example-provider/src/main/java/com/xxl/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/xxl/example/provider/EasyProviderExample.java
@@ -14,7 +14,7 @@ import com.xxl.rpc.server.VertxHttpServer;
 public class EasyProviderExample {
     public static void main(String[] args) {
         // 注册服务
-//        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
